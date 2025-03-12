35f66445ca5cdfda19a4fa7e2f1b129ff46f0bc7
Ho11i <matvey.galeev82@gmail.com>
2025-03-12 19:07:23 +0300

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,28 @@
+import telebot
+import requests
+import json
+
+bot = telebot.TeleBot('7841264202:AAHvHOTMMevaQDePOPSmVdbraZTMAa77C14')
+API = '6bcb65a80bfb8e35ef3f14194bd82bab'
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name},\nНапиши назвагие своего города!')
+
+
+@bot.message_handler(content_types=['text'])
+def get_weather(message):
+    town = message.text.strip().lower()
+    res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={town}&appid={API}&units=metric')
+    if res.status_code == 200:
+        data = json.loads(res.text)
+        bot.reply_to(message, f'Now weather: {data["main"]["temp"]}')
+    else:
+        bot.reply_to(message, "sorry, but weather in this cite unknow")
+
+
+
+
+
+
+
+bot.polling(non_stop=True)
